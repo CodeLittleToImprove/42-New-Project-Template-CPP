4a24bb255e3a0f424bbe635a7a9de7d5a24624c0
tbui-quo <tbui-quo@student.42wolfsburg.de>
2025-07-10 16:41:04 +0200

    comments default uncomment

--- ex00/Template.cpp
+++ ex00/Template.cpp
@@ -4,30 +4,30 @@
 // Default constructor
 Template::Template()
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 }
 
 // Copy constructor
 Template::Template(const Template& other)
 	:_atribute(other._atribute)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	// this->member = other.member;
 }
 
 // Copy Assignment operator overload
 Template &Template::operator=(const Template& other)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
-	// if (this != &other)
-	// 	this->member = other.member();
+	// std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->member = other.member();
 	return (*this);
 }
 
 // Destructor
 Template::~Template(void)
 {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
 
 
